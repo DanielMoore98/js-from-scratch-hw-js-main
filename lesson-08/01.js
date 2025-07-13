098161d812a321628c3cac57fdafd47a03aae0a1
DanielMoore98 <daniel.m@trabemco.net>
2025-07-13 13:37:50 +0200

    lesson-08 Q1 complete

--- lesson-08/01.js
+++ lesson-08/01.js
@@ -6,4 +6,13 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
+function capitalizeWords(str) {
+  stringArr = str.split(" ")
+  for(let i = 0; i < stringArr.length; i++){
+    let capStr = stringArr[i]
+    stringArr[i] = capStr[0].toUpperCase() + capStr.slice(1)
+  }
+  return stringArr.join(" ")
+}
+
+console.log(capitalizeWords("hello world from javascript"));
